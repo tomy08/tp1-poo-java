ebc8982c79496abdd1bb1e1b81e056742ea007b0
tomy08 <tomasesantacruz@gmail.com>
2025-06-05 20:33:00 -0300

    Add comments in ej5

--- ej5/src/ej5/ej5.java
+++ ej5/src/ej5/ej5.java
@@ -1,18 +1,17 @@
 package ej5;
+
 import java.util.Scanner;
 
 public class ej5 {
-
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Introduce un numero del 1 al 10: ");
         int num = scanner.nextInt();
-
-        if (num >= 1 && num <= 10) {
+        if (num >= 1 && num <= 10) { // Verifica que el numero este en el rango de 1 a 10
             System.out.println("Tabla de multiplicar del " + num + ":");
             for (int i = 1; i <= 10; i++) {
-                System.out.println(num + " x " + i + " = " + (num * i));
+                System.out.println(num + " x " + i + " = " + (num * i)); // Imprime la tabla de multiplicar.
             }
         } else {
             System.out.println("Error: Introduce un numero entre 1 y 10.");
@@ -20,6 +19,5 @@ public class ej5 {
 
         scanner.close();
 
-	}
-
+    }
 }
