16842341ea7e2c5cb7f5824404091b987ac97f0c
tomy08 <tomasesantacruz@gmail.com>
2025-06-05 20:33:13 -0300

    Add comments in ej6

--- ej6/src/ej6/ej6.java
+++ ej6/src/ej6/ej6.java
@@ -1,14 +1,17 @@
 package ej6;
+
 import java.util.Scanner;
+
 public class ej6 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Ingresar una frase/palabra: ");
-		String texto = sc.nextLine();
-		
-		String textoInvertido = new StringBuilder(texto).reverse().toString();
-		
+		String texto = sc.nextLine(); // Lee la frase o palabra ingresada por el usuario
+
+		String textoInvertido = new StringBuilder(texto).reverse().toString(); // Invierte el texto usando StringBuilder
+																				// y su metodo reverse
+
 		System.out.println("El texto convertido es: " + textoInvertido);
 
 		sc.close();
