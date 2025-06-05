983366141b39c65c7b6bc8f885ec613766e6984b
tomy08 <tomasesantacruz@gmail.com>
2025-06-05 20:32:44 -0300

    Add comments in ej3.java

--- ej3/src/ej3/ej3.java
+++ ej3/src/ej3/ej3.java
@@ -1,30 +1,30 @@
 package ej3;
-import java.util.Scanner;
+
+import java.util.Scanner; // Importa la clase Scanner para leer datos del usuario
 
 public class ej3 {
-	
+
 	public static boolean esPrimo(int num) {
-		for (int i = 2; i < num; i++) {
-			if (num%i == 0) {
+		for (int i = 2; i < num; i++) { // Busca si hay algun divisor distinto de 1 y de si mismo
+			if (num % i == 0) { // Si encuentra un divisor exacto
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); // Crea un objeto Scanner para leer desde teclado
 		int num = 0;
-		
+
 		do {
-			System.out.println("Ingresar numero mayor a 100 y primo");
-			num = sc.nextInt();
-			
-		}
-		while (!(esPrimo(num) && num>100));
-		System.out.println("El numero " + num + " es primo y mayor a 100");
-		
-		sc.close();
+			System.out.println("Ingresar numero mayor a 100 y primo"); // Pide un numero al usuario
+			num = sc.nextInt(); // Lee el numero ingresado
+
+		} while (!(esPrimo(num) && num > 100)); // Repite hasta que sea primo y mayor a 100
+		System.out.println("El numero " + num + " es primo y mayor a 100"); // Confirma el resultado
+
+		sc.close(); // Cierra el Scanner
 	}
 
 }
