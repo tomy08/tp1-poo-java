7f909a31d35086703ccec68390f8a21e8c4c9a7a
tomy08 <tomasesantacruz@gmail.com>
2025-06-05 20:33:19 -0300

    Add comments in ej7

--- ej7/src/ej7/ej7.java
+++ ej7/src/ej7/ej7.java
@@ -3,26 +3,27 @@ package ej7;
 import java.util.Scanner;
 
 public class ej7 {
-	
-	public static boolean esVocal(char letra, char[] vocales) {
-        for (char v : vocales) {
-            if (letra == v) {
+    // Verifica si un caracter es una vocal
+    public static boolean esVocal(char letra, char[] vocales) { //
+        for (char v : vocales) { // Recorre el arreglo de vocales
+            if (letra == v) { // Si la letra es igual a alguna vocal
                 return true;
             }
         }
         return false;
     }
-	
-	public static void main(String[] args) {
-		char vocales[] = {'a', 'e', 'i', 'o', 'u'};
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Ingresar una frase/palabra: ");
-		String texto = sc.nextLine().toLowerCase();
-		int cantVocales = 0;
-		int cantConsonantes = 0;
-		
-        for (char letra : texto.toCharArray()) {
-            if (Character.isLetter(letra)) {
+
+    public static void main(String[] args) {
+        char vocales[] = { 'a', 'e', 'i', 'o', 'u' };
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresar una frase/palabra: ");
+        String texto = sc.nextLine().toLowerCase(); // Lee la frase o palabra ingresada por el usuario y la convierte a
+                                                    // minúsculas
+        int cantVocales = 0;
+        int cantConsonantes = 0;
+
+        for (char letra : texto.toCharArray()) { // Recorre cada caracter de la cadena de texto
+            if (Character.isLetter(letra)) { // Verifica si el caracter es una letra
                 if (esVocal(letra, vocales)) {
                     cantVocales++;
                 } else {
@@ -33,8 +34,8 @@ public class ej7 {
 
         System.out.println("Cantidad de vocales: " + cantVocales);
         System.out.println("Cantidad de consonantes: " + cantConsonantes);
-        
+
         sc.close();
-	}
+    }
 
 }
