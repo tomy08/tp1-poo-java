8610381ff39ceff7f4b8ee90869e3e0abe3b0c5e
tomy08 <tomasesantacruz@gmail.com>
2025-06-05 20:17:12 -0300

    Implement ej5 functionality

---
+++ ej5/src/ej5/ej5.java
@@ -0,0 +1,25 @@
+package ej5;
+import java.util.Scanner;
+
+public class ej5 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce un numero del 1 al 10: ");
+        int num = scanner.nextInt();
+
+        if (num >= 1 && num <= 10) {
+            System.out.println("Tabla de multiplicar del " + num + ":");
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(num + " x " + i + " = " + (num * i));
+            }
+        } else {
+            System.out.println("Error: Introduce un numero entre 1 y 10.");
+        }
+
+        scanner.close();
+
+	}
+
+}
