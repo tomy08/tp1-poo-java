ff5ee4fea231706775150e0cccb46d0f35a5f74d
tomy08 <tomasesantacruz@gmail.com>
2025-06-05 20:33:25 -0300

    Add comments in ej8

--- ej8/src/ej8/ej8.java
+++ ej8/src/ej8/ej8.java
@@ -5,35 +5,38 @@ import java.util.Random;
 
 public class ej8 {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Random rnd = new Random();
-		int numAAdivinar = rnd.nextInt(100) + 1;
-		int guess = 0;
-		int guesses = 0;
-		
-		do {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rnd = new Random(); // Crea una instancia de Random para generar números aleatorios
+        int numAAdivinar = rnd.nextInt(100) + 1;
+        int guess = 0;
+        int guesses = 0;
+
+        do {
             System.out.println("Ingresar numero entre 1 y 100:");
             guess = sc.nextInt();
 
+            // Verifica que el número ingresado esté dentro del rango permitido
             if (guess < 1 || guess > 100) {
                 System.out.println("Número invalido, debe estar entre 1 y 100.");
-                continue; 
+                continue;
             }
 
+            // Compara el número a adivinar con el número ingresado
             if (numAAdivinar > guess) {
                 System.out.println("El numero a adivinar es mayor.");
             } else if (numAAdivinar < guess) {
                 System.out.println("El numero a adivinar es menor.");
             }
 
+            // Incrementa el contador de intentos
             guesses++;
 
         } while (numAAdivinar != guess);
 
         System.out.println("¡Adivinaste el número en " + guesses + " intentos.");
-		
-		sc.close();
-	}
+
+        sc.close();
+    }
 
 }
